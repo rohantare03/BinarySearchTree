6930d399314a7d4be6fb872769be562f71eceb6a
rohantare03 <rohatare123@gmail.com>
2022-05-26 20:13:44 +0530

    [Rohan] Add : Creating a Binary Tree

---
+++ BinaryTree/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinarySearchTree<K> where K : IComparable
+    {
+        public K NodeData;
+        public BinarySearchTree<K> Left;
+        public BinarySearchTree<K> Right;
+        public BinarySearchTree(K Nodedata)
+        {
+            this.NodeData = Nodedata;
+            this.Left = null;
+            this.Right = null;
+        }
+        public void AddNode(K item)
+        {
+            K CurrentNode = NodeData;   //56
+            if (item.CompareTo(CurrentNode) < 0)
+            {
+                if (this.Left == null)
+                {
+                    Left = new BinarySearchTree<K>(item);
+                }
+                else
+                {
+                    Left.AddNode(item);
+                }
+                
+            }
+            else
+            {
+                if (this.Right == null)
+                {
+                    Right = new BinarySearchTree<K>(item);
+                }
+                else
+                {
+                    Right.AddNode(item);
+                }
+               
+            }
+        }
+        public void Display()
+        {
+            if (this.Left != null)
+            {
+                Left.Display();
+            }
+            Console.Write("  ");
+            Console.Write(this.NodeData);
+            Console.Write("  ");
+            if (this.Right != null)
+            {
+                Right.Display();
+            }
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace BinaryTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.AddNode(30);
+            binarySearchTree.AddNode(70);
+        
+            binarySearchTree.Display();
+           
+        }
+    }
+}
